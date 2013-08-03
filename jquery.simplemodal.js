3e44ecd0cf07ec3b42fdbef5f4d86cb0b01cb6a3
John Deerhake <jdeerhake@gmail.com>
2013-08-03 16:17:43 -0700

    Add jshintrc and fix issues with that.  Clean up/simplify event triggering. Remove separation of binding and init.

--- jquery.simplemodal.js
+++ jquery.simplemodal.js
@@ -5,16 +5,18 @@
  * Licensed under the MIT license
  */
 
-;(function ( $ ) {
-  var backdrop,
-    FADE_TIME = 300,
+;(function( $ ) {
+  var FADE_TIME = 300,
     defaults = {
       "class" : "simple-modal-box"
-    };
+    },
+    backdrop = (function() {
+      var bd = false;
+      return function() {
+        return bd || ( bd = $( "<div />", { "class" : "simple-modal-backdrop" }).appendTo( "body" ) );
+      };
+    }());
 
-  function createBackdrop() {
-    backdrop = $("<div />", { "class" : "simple-modal-backdrop" }).appendTo("body");
-  }
 
   function Modal ( el, settings ) {
     $.extend( this, settings );
@@ -25,59 +27,45 @@
 
   Modal.prototype = {
     init : function () {
-      if(!backdrop) {
-        createBackdrop();
-      }
       this.modal = {};
-      this.modal.el = $("<div />", { "class" :  this["class"] }).appendTo("body");
+      this.modal.el = $( "<div />", { "class" :  this[ "class" ] }).appendTo( "body" );
       this.modal.el
         .append( this.el.remove().show() )
         .css({ left: 0, top: 0, position: "fixed" }); /* Legacy fix */
-      this.bindings();
-    },
-    bindings : function () {
+
       this.el.on( "show",   $.proxy( this, "show" ) );
       this.el.on( "hide",   $.proxy( this, "hide" ) );
       this.el.on( "change", $.proxy( this, "center" ) );
 
+      var triggerShow = $.proxy( this.el, "trigger", "show" ),
+          triggerHide = $.proxy( this.el, "trigger", "hide" );
 
-      var triggerShow = $.proxy( function() {
-        this.el.trigger("show");
-        return false;
-      }, this ),
-      triggerHide = $.proxy( function() {
-        this.el.trigger("hide");
-        return false;
-      }, this );
-
-      backdrop.on( "click", triggerHide );
+      this.el.find( ".close" ).on( "click", triggerHide );
+      backdrop().on( "click", triggerHide );
       this.el.on( "click", ".close", triggerHide );
+      if( this.trigger ) { this.trigger.on( "click", triggerShow ); }
 
       $( document ).on( "keyup", function(ev) {
-        if( ev.keyCode === 27 ) { triggerHide(); } /*ESC*/
-      });
-
-      if( this.trigger ) {
-        this.trigger.on( "click", triggerShow );
-      }
-
-      this.el.find(".close").on( "click" );
+        if( ev.keyCode === 27 ) { triggerHide(); } /* ESC */
+      } );
     },
     show : function () {
       if( this.isShown ) { return false; }
       this.isShown = true;
-      backdrop.show();
+      backdrop().show();
       this.modal.el.css({ opacity : 0 }).show();
       this.center();
-      this.modal.el.animate({ opacity : 1 }, FADE_TIME);
+      this.modal.el.animate({ opacity : 1 }, FADE_TIME, $.proxy(function() {
+        this.el.trigger( "shown" );
+      }, this ) );
       return false;
     },
     hide : function () {
       if( !this.isShown ) { return false; }
       this.isShown = false;
-      backdrop.fadeOut(FADE_TIME);
+      backdrop().fadeOut( FADE_TIME );
       this.modal.el.fadeOut(FADE_TIME, $.proxy( function () {
-        this.el.trigger("hidden");
+        this.el.trigger( "hidden" );
       }, this ));
       return false;
     },
@@ -93,7 +81,7 @@
   };
 
   $.fn.simpleModal = function ( options ) {
-    var settings = $.extend({}, defaults, options);
+    var settings = $.extend( {}, defaults, options );
     return this.each(function() {
       new Modal ( this, settings );
     });
